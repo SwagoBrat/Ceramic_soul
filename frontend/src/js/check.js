3cf6e9594f74a7ff457410cbc62fe44db9dd0658
guzzzlikz <jaroslavvolk9@gmail.com>
2025-09-22 22:41:37 +0300

    check.js fix (message Failed to fetch doesn't pop up now)

--- frontend/src/js/check.js
+++ frontend/src/js/check.js
@@ -1,12 +1,8 @@
 check()
 async function check() {
-    try {
-        const response = await fetch("http://localhost:8080/api/check")
-        const name = await response.text();
-        if (response.ok) {
-            document.getElementById("user-info").textContent = name;
-        }
-    } catch (e) {
-        alert(e)
+    const response = await fetch("http://localhost:8080/api/check")
+    const name = await response.text();
+    if (response.ok) {
+        document.getElementById("user-info").textContent = name;
     }
 }
